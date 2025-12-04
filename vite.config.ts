77b76f7694937d2cdb522902fe80dd5dca70406e
shahdhammouda02 <99508416+shahdhammouda02@users.noreply.github.com>
2025-12-04 14:24:49 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// @ts-ignore
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
-  base: "./"
+  base: "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['apexcharts', 'react-apexcharts'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  }
 });
